562666ff3f1e6ed243872c9d9756f2e17fb4956f
chenming <583751490@qq.com>
2022-10-18 15:24:51 +0800

    图片下载分类：1、Api下载；2、随机链接下载

--- img-get-from-iw233.php
+++ img-get-from-iw233.php
@@ -1,81 +1,108 @@
 <?php
 /*
  * +----------------------------------------------------------------------
- * |  图片盲盒自动下载
+ * |  iw233 全api图片批量下载
  * |  基于http://api.iw233.cn/API/index.php
- * |  https://iw233.cn/API/Random.php 色图盲盒，0.05%概率出露点瑟图(每4000张稳定井一张)
+ * |  图片链接随机获取，不是设置了100就一定是下载100张图片，可能当前获取的图片中部分是已下载过的
  * +----------------------------------------------------------------------
  */
 
-//获取实际图片连接
-function get_redirect_url_by_header($url)
-{
-  $header = get_headers($url, 1);
-  if (strpos($header[0], '301') !== false || strpos($header[0], '302') !== false) {
-    if (is_array($header['Location'])) {
-      return $header['Location'][count($header['Location']) - 1];
-    } else {
-      return $header['Location'];
-    }
-  } else {
-    return $url;
-  }
-}
-
-//保存图片
-function down_img_by_url($url, $name, $dir = '')
-{
-  if (empty($dir)) $dir = date('Y-m-d');
-  $dirname = './img-down-temp/' . $dir;//检查文件夹是否存在，不存在则创建
-
-  if (is_dir($dirname) || @mkdir($dirname)) {
-    $fullName = './img-down-temp/' . $dir . '/' . $name;
-    if (!file_exists($fullName)) {
-
-      #region func 1
-      /*$content = file_get_contents($url);
-      file_put_contents($fullName, $content);*/
-      #endregion
-
-      #region func 2
-      $in = fopen($url, "rb");
-      $out = fopen($fullName, "wb");
-      while ($chunk = fread($in, 8192)) {
-        fwrite($out, $chunk, 8192);
-      }
-      fclose($in);
-      fclose($out);
-      #endregion
-
-      return true;
-    }
-  }
-
-  return false;
-}
-
-//自动保存
 /**
- * @param int $_count_max 设置本次最大下载量
+ * curl get
+ * @param $url
+ * @return bool|string
+ */
+function curl_get($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  $output = curl_exec($curl);
+  curl_close($curl);
+  return $output;
+}
+
+/**
+ * 自动下载
+ * @param string $sort api图片类型
+ * @param int $num 当前下载图片张数，最大值100
  * @return int
  */
-function down_img_count($_count_max = 50)
+function down_img_by_api($sort, $num = 100)
 {
-  $_count = 0;
-  while ($_count < $_count_max) {
-    $_url_api = 'https://iw233.cn/API/Random.php';
-    $_url_pic = get_redirect_url_by_header($_url_api);//获取图片完整连接
-    $_name_pic = basename($_url_pic);//获取图片文件名
-    down_img_by_url($_url_pic, $_name_pic, 'iw233');//保存图片至本地文件夹
-    $_count++;
-    sleep(0.7);//设置请求延时,防止拉黑ip
+  $count = 0;
+
+  //图片网站图片类型汇总
+  $_arr_api_sort = [
+    ['sort' => 'random', 'text' => '随机壁纸（全部图）'],
+    ['sort' => 'iw233', 'text' => '随机壁纸（无色图）'],
+    ['sort' => 'top', 'text' => '壁纸推荐'],
+    ['sort' => 'yin', 'text' => '银发'],
+    ['sort' => 'cat', 'text' => '兽耳'],
+    ['sort' => 'xing', 'text' => '星空'],
+    ['sort' => 'mp', 'text' => '竖屏壁纸'],
+    ['sort' => 'pc', 'text' => '横屏壁纸']
+  ];
+
+  # api接口汇总
+  //$_api_host = 'https://iw233.cn/api.php';      # 60秒120次GET,超过拉黑IP
+  //$_api_host = 'https://api.iw233.cn/api.php';  # 并发连接：100，超过404
+  //$_api_host = 'https://ap1.iw233.cn/api.php';  # 并发连接：100，超过404
+  $_api_host = 'https://dev.iw233.cn/api.php';  # 并发连接：80， 超过404
+
+  $_info_sort = [];
+  foreach ($_arr_api_sort as $_sort) {
+    if ($_sort['sort'] == $sort) {
+      $_info_sort = $_sort;
+      break;
+    }
   }
 
-  return $_count;
+  if ($_info_sort) {
+    $_url_api = $_api_host . '?sort=' . $_info_sort['sort'] . '&num=' . $num . '&type=json';
+
+    $_str_json = curl_get($_url_api);
+    $_info_pic = json_decode($_str_json, true);
+
+    if ($_info_pic) {
+      $_list_pic_url = $_info_pic['pic'];
+      $dirname = './img-down-temp/' . $_info_sort['text'];//检查文件夹是否存在，不存在则创建
+
+      if (is_dir($dirname) || @mkdir($dirname)) {
+
+        foreach ($_list_pic_url as $_url_pic) {
+          $_name_pic = basename($_url_pic);//获取图片文件名
+          $fullName = $dirname . '/' . $_name_pic;
+          if (!file_exists($fullName)) {
+
+            #region func 1
+            /*$content = file_get_contents($url);
+            file_put_contents($fullName, $content);*/
+            #endregion
+
+            #region func 2
+            $in = fopen($_url_pic, "rb");
+            $out = fopen($fullName, "wb");
+            while ($chunk = fread($in, 8192)) {
+              fwrite($out, $chunk, 8192);
+            }
+            fclose($in);
+            fclose($out);
+            #endregion
+
+            $count++;
+          }
+        }
+      }
+    }
+  }
+
+  return $count;
 }
 
+
 echo '开始下载时间：' . date('Y-m-d H:i:s');
-$_count_img = down_img_count();
+$_count_img = down_img_by_api('pc');
 echo '<br>完成下载时间：' . date('Y-m-d H:i:s');
 echo '<br>合计下载图片' . $_count_img . '张';
 
---
+++ img-get-from-random.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * +----------------------------------------------------------------------
+ * |  图片盲盒自动下载，可适用于其他随机图片下载
+ * |  基于 https://iw233.cn/API/Random.php 色图盲盒，0.05%概率出露点瑟图(每4000张稳定井一张)
+ * +----------------------------------------------------------------------
+ */
+
+//获取实际图片连接
+function get_redirect_url_by_header($url)
+{
+  $header = get_headers($url, 1);
+  if (strpos($header[0], '301') !== false || strpos($header[0], '302') !== false) {
+    if (is_array($header['Location'])) {
+      return $header['Location'][count($header['Location']) - 1];
+    } else {
+      return $header['Location'];
+    }
+  } else {
+    return $url;
+  }
+}
+
+//保存图片
+function down_img_by_url($url, $name, $dir = '')
+{
+  if (empty($dir)) $dir = date('Y-m-d');
+  $dirname = './img-down-temp/' . $dir;//检查文件夹是否存在，不存在则创建
+
+  if (is_dir($dirname) || @mkdir($dirname)) {
+    $fullName = './img-down-temp/' . $dir . '/' . $name;
+    if (!file_exists($fullName)) {
+
+      #region func 1
+      /*$content = file_get_contents($url);
+      file_put_contents($fullName, $content);*/
+      #endregion
+
+      #region func 2
+      $in = fopen($url, "rb");
+      $out = fopen($fullName, "wb");
+      while ($chunk = fread($in, 8192)) {
+        fwrite($out, $chunk, 8192);
+      }
+      fclose($in);
+      fclose($out);
+      #endregion
+
+      return true;
+    }
+  }
+
+  return false;
+}
+
+//自动保存
+/**
+ * @param int $_count_max 设置本次最大下载量
+ * @return int
+ */
+function down_img_count($_count_max = 50)
+{
+  $_count = 0;
+  while ($_count < $_count_max) {
+    $_url_api = 'https://iw233.cn/API/Random.php';
+    $_url_pic = get_redirect_url_by_header($_url_api);//获取图片完整连接
+    $_name_pic = basename($_url_pic);//获取图片文件名
+    down_img_by_url($_url_pic, $_name_pic, 'iw233-' . date('Ymd'));//保存图片至本地文件夹
+    $_count++;
+    sleep(0.7);//设置请求延时,防止拉黑ip
+  }
+
+  return $_count;
+}
+
+echo '开始下载时间：' . date('Y-m-d H:i:s');
+$_count_img = down_img_count();
+echo '<br>完成下载时间：' . date('Y-m-d H:i:s');
+echo '<br>合计下载图片' . $_count_img . '张';
+
